058252a09c3e734bc01deeb055c69459b746c18e
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-08-16 01:44:10 -0400

    Trying everything

--- processor.py
+++ processor.py
@@ -1,42 +1,16 @@
-import boto3
-from flask import Flask, request, redirect
-import stripe
-
-# ENCRYPTED_STRIPE_KEY = os.environ['ENCRYPTED_STRIPE_KEY']
-# # Decrypt code should run once and variables stored outside of the function
-# # handler so that these are decrypted once per container
-# DECRYPTED = boto.client('kms') decrypt(CiphertextBlob=b64decode(ENCRYPTED_STRIPE_KEY))['Plaintext']
-# stripe.api_key = DECRYPTED
+import logging
+from flask import Flask
 
 app = Flask(__name__)
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-@app.route("/", methods=["POST"])
-def handler(event, context):
-    try:
-        print(context)
-        print(request.form.get('event'))
+@app.route('/', methods=['GET', 'POST'])
+def lambda_handler(event=None, context=None):
+    logger.info('Lambda function invoked index()')
 
-        customer = stripe.Customer.create(
-            email=request.form.get('stripeEmail'),
-            source=request.form.get('stripeToken')
-        )
+    return 'hello from Flask!'
 
-        stripe.Subscription.create(
-            customer=customer.id,
-            plan="base-plan"
-        )
-
-        # return redirect("https://www.your-site/success-message")
-        response = {
-            "statusCode": 200,
-            "body": json.dumps(event)
-        }
-
-        return response
-
-    except Exception as e:
-        # Don't forget to log your errors before doing this!
-        return redirect("https://www.your-site/error-message")
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
