d6143bc21ee99059db3f14c5e40dc75de6729797
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-08-27 23:48:45 -0400

    Getting this craziness sorted out

---
+++ src/handler.py
@@ -0,0 +1,32 @@
+import json
+import urllib.parse
+import stripe
+
+def charge(event, context):
+
+    body = event['body']
+    encoded_form = urllib.parse.unquote(body)
+    decoded_form = urllib.parse.parse_qs(encoded_form)
+
+    token = decoded_form["token"][0]
+    email = decoded_form["email"][0]
+
+    charge = stripe.Charge.create(
+        amount=100,
+        currency="usd",
+        description="One reminder",
+        source=token,
+    )
+
+    body = {
+        "message": "Charging credit card",
+        "token": token,
+        "email": email
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
