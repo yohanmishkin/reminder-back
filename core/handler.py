99177cb400b55c9939dfe0f16a3e8b03095a8d3c
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-09-15 00:14:55 -0400

    Fixed import

--- core/handler.py
+++ core/handler.py
@@ -1,22 +1,16 @@
 import json
+import urllib.parse
 import os
 import stripe
-import urllib.parse
+from core.objects import S3Object, Polly
 
 stripe.api_key = os.environ['STRIPE_KEY']
+bucket_name = os.environ['AUDIO_BUCKET']
+
 
 def charge(event, context):
 
-    encoded_form = urllib.parse.unquote(event['body'])
-    decoded_form = urllib.parse.parse_qs(encoded_form)
-
-    token = decoded_form["token"][0]
-    email = decoded_form["email"][0]
-    message = decoded_form["message"][0]
-    phone = decoded_form["phone"][0]
-    cron = decoded_form["cron"][0]
-
-    bucket_name = os.environ['AUDIO_BUCKET']
+    email, message, token, phone, cron = unpack_data(event)
 
     url = S3Object(
         bucket_name,
@@ -38,7 +32,10 @@ def charge(event, context):
     body = {
         "message": "Charging credit card",
         "token": token,
-        "email": email
+        "email": email,
+        "phone": phone,
+        "cron": cron,
+        "recording_url": url
     }
 
     response = {
@@ -47,3 +44,13 @@ def charge(event, context):
     }
 
     return response
+
+def unpack_data(event):
+    encoded_form = urllib.parse.unquote(event['body'])
+    decoded_form = urllib.parse.parse_qs(encoded_form)
+    token = decoded_form["token"][0]
+    email = decoded_form["email"][0]
+    message = decoded_form["message"][0]
+    phone = decoded_form["phone"][0]
+    cron = decoded_form["cron"][0]
+    return email, message, token, phone, cron
